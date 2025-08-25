ac20a163559a08d7ca4c76607f861c5aa110b4e0
MrEggy02 <Akavin05960@gmail.com>
2025-08-25 16:48:28 +0700

    Update txt_personal_widget.dart

--- lib/feature/usermode/presentation/setting/page/person/widget/txt_personal_widget.dart
+++ lib/feature/usermode/presentation/setting/page/person/widget/txt_personal_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:m9/core/config/theme/app_color.dart';
 
-class TxtPersonalWidget extends StatefulWidget {
+class TxtPersonalWidget extends StatelessWidget {
   final TextEditingController controller;
   final IconData icon;
   final String name;
   final TextInputType type;
+
   const TxtPersonalWidget({
     super.key,
     required this.controller,
@@ -14,11 +15,6 @@ class TxtPersonalWidget extends StatefulWidget {
     required this.type,
   });
 
-  @override
-  State<TxtPersonalWidget> createState() => _TxtPersonalWidgetState();
-}
-
-class _TxtPersonalWidgetState extends State<TxtPersonalWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,20 +25,16 @@ class _TxtPersonalWidgetState extends State<TxtPersonalWidget> {
         borderRadius: BorderRadius.circular(10),
       ),
       child: TextFormField(
-        initialValue: "",
-        onChanged: (value) {
-          setState(() {
-            widget.controller.text = value;
-          });
-        },
-        keyboardType: widget.type,
+        controller: controller, // ✅ ใช้ controller ตรง ๆ
+        keyboardType: type,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(vertical: 10),
+          contentPadding: const EdgeInsets.symmetric(vertical: 10),
           border: InputBorder.none,
-          prefixIcon: Icon(widget.icon, color: Colors.indigo.shade800),
-          hintText: widget.name,
+          prefixIcon: Icon(icon, color: Colors.indigo.shade800),
+          hintText: name,
         ),
       ),
     );
   }
 }
+
