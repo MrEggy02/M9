28a5a372cf0bb3c9429f3698b742ffa005fed050
MrEggy02 <Akavin05960@gmail.com>
2025-08-25 16:53:38 +0700

    Update auth_state.dart

--- lib/feature/auth/cubit/auth_state.dart
+++ lib/feature/auth/cubit/auth_state.dart
@@ -1,32 +1,51 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:m9/feature/auth/domain/models/bank_account_model.dart';
 import 'package:m9/feature/auth/domain/models/user_model.dart';
 
 enum AuthStatus { initial, loading, googleLoading, success, failure }
 
 class AuthState extends Equatable {
   final String? error;
-
   final AuthStatus? authStatus;
   final UserModel? userModel;
   final bool? isCheck;
-  const AuthState({this.error, this.authStatus, this.userModel, this.isCheck});
+  final BankAccount? bankAccount;
+  final bool isUpdate;
+
+  const AuthState({
+    this.error,
+    this.authStatus,
+    this.userModel,
+    this.isCheck,
+    this.bankAccount,
+    this.isUpdate = false,
+  });
+
   AuthState copyWith({
     AuthStatus? authStatus,
     String? error,
     UserModel? userModel,
     bool? isCheck,
+    BankAccount? bankAccount,
+    bool? isUpdate,
   }) {
     return AuthState(
       authStatus: authStatus ?? this.authStatus,
       userModel: userModel ?? this.userModel,
       error: error ?? this.error,
       isCheck: isCheck ?? this.isCheck,
+      bankAccount: bankAccount ?? this.bankAccount,
+      isUpdate: isUpdate ?? this.isUpdate,
     );
   }
 
   @override
-  List<Object?> get props => [error, authStatus, isCheck, userModel];
+  List<Object?> get props => [
+    error,
+    authStatus,
+    isCheck,
+    userModel,
+    bankAccount,
+    isUpdate,
+  ];
 }
